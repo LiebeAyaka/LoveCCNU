cb528912017248fdbd33814af497abe55dc6d091
LiebeAyaka <dreamsakura@outlook.com>
2024-08-03 01:04:40 +0800

    20240803-00

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "My Awesome Project",
+  title: "LoveCCNU",
   description: "A VitePress Site",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
@@ -13,16 +13,64 @@ export default defineConfig({
 
     sidebar: [
       {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+        '/入学必备/': [
+          {
+            text: '入学必备',
+            item: [
+              { text: '入学需要带的材料', link: '/入学必备/' },
+              { text: '日常生活用品', link: '/入学必备/日常生活用品' },
+              { text: '如何去华师', link: '/入学必备/如何去华师' },
+              { text: '入学考试', link: '/入学必备/入学考试' },
+              { text: '校园网', link: '/入学必备/校园网' },
+              { text: '常用app', link: '/入学必备/常用app' },
+              { text: '常用网站', link: '/入学必备/常用网站' },
+              { text: '提供意见', link: '/入学必备/提供意见或帮助我' },
+              { text: '致谢', link: '/入学必备/致谢' }
+            ]
+          }
+        ],
+
+        '/学在华大/': [
+          {
+            text: '学在华大',
+            item: [
+              { text: '首页', link: '/学在华大/' },
+              { text: '专业人培', link: '/学在华大/专业人培' },
+              { text: '学院机构', link: '/学在华大/学院机构' },
+              { text: '转专业', link: '/学在华大/转专业' }
+            ]
+          }
+        ],
+
+        '/生活娱乐/': [
+          {
+            text: '生活娱乐',
+            item: [
+              { text: '首页', link: '/生活娱乐/' },
+              { text: '吃饭篇概况', link: '/生活娱乐/吃饭篇概况' },
+              { text: '校内出行', link: '/生活娱乐/校内出行' },
+              { text: '校外公共交通', link: '/生活娱乐/校外公共交通' },
+              { text: '武汉哪里可以玩', link: '/生活娱乐/武汉哪里可以玩' }
+            ]
+          }
+        ],
+
+        '/住在华大/': [
+          {
+            text: '住在华大',
+            item: [
+              { text: '首页', link: '/住在华大/' },
+              { text: '住宿情况概要', link: '/住在华大/住宿情况概要' },
+              { text: '住宿生活改善', link: '/住在华大/住宿生活改善' },
+              { text: '医疗服务', link: '/住在华大/医疗服务' }
+            ]
+          }
         ]
       }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
     ]
-  }
+  },
+
+  socialLinks: [
+    { icon: 'github', link: 'https://github.com/LiebeAyaka/LoveCCNU' }
+  ]
 })
--- docs/index.md
+++ docs/index.md
@@ -3,23 +3,32 @@
 layout: home
 
 hero:
-  name: "My Awesome Project"
-  text: "A VitePress Site"
-  tagline: My great project tagline
+  name: "华师新生帮助"
+  text: "To help new CCNUers"
+  tagline: 一个志在帮助新生的华师文档
   actions:
     - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
+      text: 关注站主的B站
+      link: https://space.bilibili.com/473782502
     - theme: alt
-      text: API Examples
-      link: /api-examples
+      text: 通过QQ联系站主
+      link: https://tool.gljlw.com/qq/?qq=1028191914
 
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: 外院专区
+    details: 站主是外院的，有点私心不行吗
+  - title: 开学准备
+    details: 开学之前要带什么去学校呢
+    link: /入学必备/开学材料准备
+  - title: 住宿情况
+    details: 浅浅地了解一下未来的家吧
+    link: /住在华大
+  - title: 吃喝玩乐
+    details: 大学生偶尔享受享受生活不过分吧
+    link: /生活娱乐
+  - title: 学习生活
+    details: 大学生还是不能忘记自己的本职的
+    link: /学在华大
+  
 ---
 
